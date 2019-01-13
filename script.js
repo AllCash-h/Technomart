5bda08b0d731d1b6e6bdd6bb8fa3496cfa4046eb
AllCash <felo3113@gmail.com>
2019-01-13 15:29:13 +0300

    Technomart
    Finished project

---
+++ script.js
@@ -0,0 +1,5 @@
+const materials = document.querySelector('.materials');
+const popularProducts = document.querySelector('.popular-products');
+materials.classList.add('new');
+lastPerforator = popularProducts.querySelector('.perforator:last-child');
+lastPerforator.classList.add('new');
